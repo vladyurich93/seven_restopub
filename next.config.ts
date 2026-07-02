a1b8e86290624fd970a0bc6b7267e2228f8c21e2
Владислав Улянченко <vladislavulancenko@MacBook-Air-Vladislav.local>
2026-07-02 21:30:03 +0300

    Generate valid XML sitemap

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml; charset=utf-8",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+      {
         source: "/((?!sitemap.xml|robots.txt).*)",
         headers: [
           {
